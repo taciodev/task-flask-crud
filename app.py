ab188dad370a3c437370803d0801ad1d14d93f67
Taciano Da Hora <taciano.dev@outlook.com>
2024-11-22 20:18:24 -0300

    feat: routes to search for tasks, all of them and a specific one

--- app.py
+++ app.py
@@ -17,5 +17,19 @@ def create_task():
     return jsonify({'message': 'task created successfully'}), 201
 
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    total_tasks = len(task_list)
+    return jsonify({'tasks': task_list, 'total_tasks': total_tasks})
+
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+    for task in tasks:
+        if task.id == id: return jsonify(task.to_dict())
+    return jsonify({'message': 'could not find the task'}), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
